7c2c5c755ed9303bf419f3a6d5b6418b0e293d16
maviionur <onurmavi_bjk@hotmail.com>
2019-09-19 13:39:57 +0300

    Blog is created

---
+++ blogSite/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+
+    url(r'^signup/$', views.signup_view, name="signup"),
+    url(r'^login/$', views.login_view, name="login"),
+    url(r'^logout/$', views.logout_view, name="logout")
+
+
+]
